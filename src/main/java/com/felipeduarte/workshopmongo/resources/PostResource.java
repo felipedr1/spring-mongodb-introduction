829236de26f5a707c14dcb4803f9b5ae5cc84d8d
Felipe Duarte Rodrigues <felipedr3m@gmail.com>
2025-11-24 17:41:11 -0300

    Consulta com query methods

--- src/main/java/com/felipeduarte/workshopmongo/resources/PostResource.java
+++ src/main/java/com/felipeduarte/workshopmongo/resources/PostResource.java
@@ -1,10 +1,13 @@
 package com.felipeduarte.workshopmongo.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.felipeduarte.workshopmongo.domain.Post;
 import com.felipeduarte.workshopmongo.services.PostService;
@@ -21,5 +24,12 @@ public class PostResource {
 	public ResponseEntity <Post> findById(@PathVariable String id){
 		Post obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@GetMapping("/titlesearch")
+	public ResponseEntity <List<Post>> findByTitle(@RequestParam(value="text") String text){
+		text = URL.decodeParam(text);
+		List<Post> list = service.findByTitle(text);		
+		return ResponseEntity.ok().body(list);
 	}
 }
--- src/main/java/com/felipeduarte/workshopmongo/services/PostService.java
+++ src/main/java/com/felipeduarte/workshopmongo/services/PostService.java
@@ -27,6 +27,9 @@ public class PostService {
 	
 	}
 	
+	public List<Post> findByTitle(String text){
+		return repo.findByTitleContainingIgnoreCase(text);
+	}
 }
 	
 	
