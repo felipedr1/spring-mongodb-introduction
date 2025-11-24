6678503751f94e1a45e9b36d3cb5eaf94833f971
Felipe Duarte Rodrigues <felipedr3m@gmail.com>
2025-11-24 00:49:58 -0300

    Update e Delete usuário

--- src/main/java/com/felipeduarte/workshopmongo/services/UserService.java
+++ src/main/java/com/felipeduarte/workshopmongo/services/UserService.java
@@ -31,6 +31,21 @@ public class UserService {
 		return repo.insert(obj);
 	}
 	
+	public void delete(String id) {
+		repo.deleteById(id);
+	}
+	
+	public User update(User obj) {
+		User newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+	
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+	}
+
 	public User fromDto(UserDTO objDto) {
 		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
