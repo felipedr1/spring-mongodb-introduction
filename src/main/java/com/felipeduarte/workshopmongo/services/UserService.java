f7e9120a41477b3ed77fd601d972208092311ed0
Felipe Duarte Rodrigues <felipedr3m@gmail.com>
2025-11-24 00:11:53 -0300

    Obtendo usuário por id

--- src/main/java/com/felipeduarte/workshopmongo/services/UserService.java
+++ src/main/java/com/felipeduarte/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.felipeduarte.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.felipeduarte.workshopmongo.domain.User;
 import com.felipeduarte.workshopmongo.repository.UserRepository;
+import com.felipeduarte.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +18,13 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User findById(String id ) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 		
 	}
+	
+	
 }
